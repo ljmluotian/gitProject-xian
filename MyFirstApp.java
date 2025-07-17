4a44550f8299ff9d1d9ca4de03bbff18597a3b9d
ljmluotian <2571966792@qq.com>
2025-07-17 06:28:23 +0000

    this change for task2

--- MyFirstApp.java
+++ MyFirstApp.java
@@ -2,6 +2,7 @@ public class MyFirstApp {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
-		SYstem.out.println("My Frist Java project");
+		System.out.println("My Frist Java projet");//this change for task2
+		System.out.println("this java file should use in task1");
 	}
 }
